be650bd8ff7aaf97a55c621d50d2c12b8249eeea
shubham <design@artificialreality.com>
2024-06-19 19:37:37 +0530

    password generator init

--- Udemy_course/01-starting-project/src/app/app.component.ts
+++ Udemy_course/01-starting-project/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponentComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  templateUrl: './app.component.html',  // Corrected templateUrl
+  styleUrls: ['./app.component.css'],
+  imports: [HeaderComponentComponent, UserComponent, TasksComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  selectedUserId = 'u1'
+  get selectedUser(){
+    return this.users.find((user)=> user.id === this.selectedUserId)!
+  }
+
+  onSelectUser(id:string){
+    console.log('selected user with id ' + id)
+  }
+}
--- Udemy_course/01-starting-project/src/app/user/user.component.ts
+++ Udemy_course/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component,Output, EventEmitter, Input, output} from '@angular/core';
 
 
 
@@ -10,10 +10,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({required : true}) id!:string;
+  @Input({required : true}) avatar!:string;
+  @Input({required : true}) name!:string;
+  @Output() select = new EventEmitter()
 
-
-   onSelectUser() {
-
-
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+   onSelectUser(){
+    this.select.emit(this.id)
    }
 }
